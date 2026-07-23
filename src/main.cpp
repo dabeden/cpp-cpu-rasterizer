6bb6d3563ce2837e13e353f6eeb29a64a9e6ba87
Devon Bedenbaugh <dabeden@gmail.com>
2026-07-23 01:32:57 +0000

    Project 3D cube vertices to 2D screen space with perspective division

--- src/main.cpp
+++ src/main.cpp
@@ -26,12 +26,20 @@ public:
         }
     }
 
+    void setScreenSizeVars(int& screenWidth, int& screenHeight){
+        screenWidth = width; 
+        screenHeight = height;
+        return;
+    }
+
 private:
     int width, height;
     std::vector<Color> pixels;
 };
 
 struct Point2D { float x, y;};
+
+struct Point3D { float x, y, z;};
 
 float edgeFunction(const Point2D& a, const Point2D& b, const Point2D& p) {
     return (p.x - a.x) * (b.y - a.y) - (p.y - a.y) * (b.x - a.x); // returns positive/negative value based on which side of the edge p is on
@@ -62,16 +70,70 @@ void rasterizeTriangle(FrameBuffer& fb, Point2D a, Point2D b, Point2D c, Color c
     }
 }
 
+Color randomColor() {
+    return Color{
+        (uint8_t)(rand() % 256),
+        (uint8_t)(rand() % 256),
+        (uint8_t)(rand() % 256)
+    };
+}
+
+
+//project 3D point to 2D screen space using a simple perspective projection
+Point2D project(Point3D p, float screenWidth, float screenHeight) {
+    float fov = 1.0f; // just a scaling factor for now, tune by eye
+    float x2d = (p.x / p.z) * fov * screenWidth + screenWidth / 2;
+    float y2d = (p.y / p.z) * fov * screenHeight + screenHeight / 2;
+    return {x2d, y2d};
+}
+
 int main() {
 
     // define frame size, plot the 3 corners of the triangle, color of the triangle, rasterize the triangle and write to a ppm file
-    FrameBuffer fb(400, 400);
+    int width, height;
+    FrameBuffer fb(1600,1600);
+    fb.setScreenSizeVars(width, height);
+  
+    Color color{30, 20, 255};
+    srand(time(nullptr));
+
+    /* Flat triangle projection
     Point2D a{200, 50};
     Point2D b{50, 350};
     Point2D c{350, 350};
-    Color color{30, 20, 255};
+    rasterizeTriangle(fb, a, b, c, color);*/
 
-    rasterizeTriangle(fb, a, b, c, color);
+
+    Point3D vertices[8] = {
+        {-20, -20, 300}, // 0: front-top-left
+        {20, -20, 300},  // 1: front-top-right
+        {20, 20, 300},   // 2: front-bottom-right
+        {-20, 20, 300},  // 3: front-bottom-left
+        {-20, -20, 400}, // 4: back-top-left
+        {20, -20, 400},  // 5: back-top-right
+        {20, 20, 400},   // 6: back-bottom-right
+        {-20, 20, 400}   // 7: back-bottom-left
+    };
+
+    struct Triangle {int a,b,c;};
+    Triangle triangles[12]={
+        {0, 1, 3}, {0, 1, 2}, // front face
+        {4,5,6}, {4,6,7}, // back face
+        {0,1,5}, {0,5,4}, // top face
+        {2,3,7}, {2,7,6}, // bottom face
+        {0,3,7}, {0,7,4}, // left face
+        {1,2,6}, {1,6,5}  // right face
+    };
+
+    for (const Triangle& tri: triangles){
+        Point2D a2d = project(vertices[tri.a], width, height);
+        Point2D b2d = project(vertices[tri.b], width, height);
+        Point2D c2d = project(vertices[tri.c], width, height);
+        rasterizeTriangle(fb, a2d, b2d, c2d, randomColor());
+    }
+
+
+
     fb.writePPM("triangle.ppm");
     return 0;
 }
